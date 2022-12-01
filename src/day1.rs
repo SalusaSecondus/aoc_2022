32e68fccea68bc05bc6b60912db29a1d5247413e
Greg Rubin <ga_rubin@apple.com>
2022-12-01 14:44:40 -0800

    Add day1 tests

--- src/day1.rs
+++ src/day1.rs
@@ -46,3 +46,25 @@ fn part2(input: &Input) -> Result<Output> {
 
     Ok(result)
 }
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const INPUT_STR: &str = "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000";
+
+    #[test]
+    fn part1_test() -> Result<()> {
+        let input = input_generator(INPUT_STR)?;
+        assert_eq!(part1(&input)?, 24000);
+        Ok(())
+    }
+
+    #[test]
+    fn part2_test() -> Result<()> {
+        let input = input_generator(INPUT_STR)?;
+        assert_eq!(part2(&input)?, 45000);
+        Ok(())
+    }
+}
